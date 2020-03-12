3aa27dcee74b988afaa0149de2fdf22506b356b9
沫雨 <1291953408@qq.com>
2020-03-12 10:01:56 +0800

    add recovery

--- gee.go
+++ gee.go
@@ -90,6 +90,13 @@ func New() *Engine {
 	return engine
 }
 
+// Default use Logger() & Recovery middlewares
+func Default() *Engine {
+	engine := New()
+	engine.Use(Logger(), Recovery())
+	return engine
+}
+
 func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
 	engine.funcMap = funcMap
 }
